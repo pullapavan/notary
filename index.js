7cef642d944d78dec00338c769f2a4db774c8d05
pullapavan <pavankumar.pulla@gmail.com>
2021-05-09 14:03:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,4 +7,4 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.listen(port, () => console.log(`url-shortener listening on port ${port}!`));
+app.listen(process.env.PORT || 5000, () => console.log(`url-shortener listening on port ${port}!`));
